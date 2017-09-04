f81c8c548aae8dabdf69d413a7ff254c31cf8852
Quentin McDonald <Quentin.McDonald@schrodinger.com>
2017-09-05 11:47:58 +1200

    Added parsing of Form POST data

--- cgi/plot.py
+++ cgi/plot.py
@@ -5,9 +5,6 @@ import os,sys
 import cgi
 import cgitb; cgitb.enable()
 
-fields = [ "temperature", "humidity", "pressure", "battery_voltage",
-          "panel_current", "load_current", "wind_speed", "rainfall", "wind_direction"]
-
 
 from table import Table
 from page import Page
@@ -15,19 +12,56 @@ from form import Form
 from formselect import FormSelect
 
 
-p = Page("GardenLab Plotting")
-
-f = Form("/cgi/plot.py","POST","Generate Plot")
-fs = FormSelect("field",fields,"temperature", "Data to plot:")
-
-f.addContent(fs)
-
-
-p.addContent(f)
+fields = [ "temperature", "humidity", "pressure", "battery_voltage",
+          "panel_current", "load_current", "wind_speed", "rainfall", "wind_direction"]
 
 
 
-print(p)
+def getFormValues(form):
+    """
+    Returns a dictionary with either the values sent by the form or default values
+    for the various form elements
+    """
+    
+    d = {}
+    if "field" in form.keys():
+        d["field"] = form["field"].value
+    else:
+        d["field"] = fields[0]
+    
+    return d
 
 
+
+
+
+def showPage():
+
+    #Deals with inputing data into python from the html form
+    form = cgi.FieldStorage()
+    
+    
+    form_settings = getFormValues(form)
+    
+    
+    
+    p = Page("GardenLab Plotting")
+    
+    f = Form("/cgi/plot.py","POST","Generate Plot")
+    fs = FormSelect("field",fields,form_settings["field"], "Data to plot:")
+    
+    f.addContent(fs)
+    
+    
+    p.addContent(f)
+    
+    
+    
+    print(p)
+    print(form_settings["field"])
+
+
+
+if __name__ == "__main__":
+    showPage()
 
