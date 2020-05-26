e397710a2ecf75f8cb2d2c3a6ffb7f24f1432e02
Quentin McDonald <dqmcdonald@gmail.com>
2020-05-26 20:07:52 +1200

    Calculate dates so that the current day from midnight is plotted.

--- cgi/CASH.py
+++ cgi/CASH.py
@@ -19,15 +19,26 @@ cgitb.enable()
 STANDARD_SAMPLING_RATE = 18.7667
 BASE_DIR = "/home/pi/jamaseisData/CASH/"
 
-def get_file_name( date ):
+def get_file_names( date ):
 
     local_time = pytz.timezone("Pacific/Auckland")
-    naive_datefime = datetime.datetime.strptime(date, "%Y-%m-%d")
-    local_datetime=local_time.localize(naive_datefime, is_dst=None)
+    naive_datetime = datetime.datetime.strptime(date, "%Y-%m-%d")
+    naive_datetime = naive_datetime + datetime.timedelta(days=1 )
+    local_datetime=local_time.localize(naive_datetime, is_dst=None)
     utc_datetime = local_datetime.astimezone(pytz.utc)
 
-    return os.path.join(BASE_DIR,str(utc_datetime.year),
-        str(utc_datetime.month), str(utc_datetime.day) ,'*.sac')
+    yesterday_utc = utc_datetime - datetime.timedelta(days=1)
+
+    streams = []
+    for i in range(12,24):
+        streams.append(os.path.join(BASE_DIR,str(yesterday_utc.year),
+            str(yesterday_utc.month), str(yesterday_utc.day) ,
+            '{}.sac'.format(i)))
+    for i in range(12):
+        streams.append(os.path.join(BASE_DIR,str(utc_datetime.year),
+            str(utc_datetime.month), str(utc_datetime.day) ,
+            '{}.sac'.format(i)))
+    return streams
    
 
 
@@ -36,7 +47,8 @@ def plot_data( st, scale, date ):
     Plot the data and return the plot as a string
     """
     fig = plt.figure()
-    title = "CASH Seismograph data for {}".format(date)
+    dt = datetime.datetime.strptime(date, "%Y-%m-%d")
+    title = "CASH Seismograph Data for {}".format(dt.strftime("%d-%m-%Y"))
     data=st.plot(figure=fig, format='png', type='dayplot', 
         interval=60, right_vertical_labels=False,
         vertical_scaling_range=scale, one_tick_per_line=True,
@@ -53,10 +65,12 @@ env = Environment(
    ) 
 
 form = cgi.FieldStorage()
+
 if "date" in form:
    default_date =  form["date"].value
 else:
-   default_date = (datetime.datetime.today()).strftime('%Y-%m-%d')
+   today = datetime.datetime.today()
+   default_date = today.strftime('%Y-%m-%d')
 
 if "scale" in form:
    default_scale =  form["scale"].value
@@ -70,16 +84,29 @@ print("")
 
 context_dict = {}
 
-file_name = get_file_name( default_date )
+file_names = get_file_names( default_date )
 
-st = read(file_name)
-# Standardize the sampling rate otherwise plotting complains:
-for tr in st:
-    tr.stats.sampling_rate = STANDARD_SAMPLING_RATE
+streams = []
+for f in file_names:
+    try:
+        st = read(f)
+        for tr in st:
+            tr.stats.sampling_rate = STANDARD_SAMPLING_RATE
+        streams.append(st)
+    except Exception:
+        pass
 
+if len(streams):
+    st = streams[0]
+if len(streams) > 1:
+    for s in streams[1:]:
+        st += s
+
+if streams:
+    context_dict["plot_string"] = plot_data(st, int(default_scale), 
+            default_date)
 context_dict["default_scale"] = default_scale
 context_dict["default_day"] = default_date
-context_dict["plot_string"] = plot_data(st, int(default_scale), default_date)
 
 
 template = env.get_template('CASH.html')
